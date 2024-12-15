dc3130ba6f34220c3218fd61ecc8e70cf5f858a1
behindy <solme47@gmail.com>
2024-12-15 13:34:00 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 1089.69 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -0,0 +1,64 @@
+def crash_check(path, t):
+    positions = {}  # 각 위치별 로봇 수를 기록할 딕셔너리
+    crash_count = 0
+    
+    # 각 로봇의 t시점 위치 확인
+    for robot_idx in range(len(path)):
+        # 해당 로봇이 아직 이동 중인 경우만 체크
+        if t < len(path[robot_idx]):
+            current_pos = tuple(path[robot_idx][t])  # 리스트는 딕셔너리 키로 사용할 수 없어서 튜플로 변환
+            
+            # 위치별 로봇 수 카운트
+            if current_pos in positions:
+                # 처음으로 2대가 되는 경우에만 카운트 증가
+                if positions[current_pos] == 1:
+                    crash_count += 1
+                positions[current_pos] += 1
+            else:
+                positions[current_pos] = 1
+                
+    return crash_count
+
+def p_length(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+def min_path(route, points):
+    result = []
+    
+    # 시작점 기록
+    result.append(points[route[0]-1])  # points 인덱스는 0부터, route의 포인트 번호는 1부터 시작
+    
+    # 각 경유 지점별로 경로 계산
+    for i in range(len(route)-1):
+        current = points[route[i]-1]
+        next_point = points[route[i+1]-1]
+        
+        # 현재 위치에서 다음 위치까지 매 초마다의 좌표 계산
+        while current != next_point:
+            # r 좌표 먼저 이동
+            if current[0] < next_point[0]:
+                current = [current[0] + 1, current[1]]
+            elif current[0] > next_point[0]:
+                current = [current[0] - 1, current[1]]
+            # r 좌표 이동이 끝났으면 c 좌표 이동
+            elif current[1] < next_point[1]:
+                current = [current[0], current[1] + 1]
+            elif current[1] > next_point[1]:
+                current = [current[0], current[1] - 1]
+                
+            result.append(current[:])
+    return result
+    
+def solution(points, routes):
+    answer = 0
+    max_time = 0
+    
+    path = []
+    for i in range(len(routes)):
+        robot_path = min_path(routes[i], points)
+        path.append(robot_path)
+        max_time = max(max_time, len(robot_path))  
+    for t in range(max_time):
+        answer += crash_check(path, t)
+    
+    return answer
