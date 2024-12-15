789de53c509ffd801fc203d73a1fc4f9b615cbb6
behindy <solme47@gmail.com>
2024-12-15 11:51:38 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1870.42 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -0,0 +1,33 @@
+def checkLimit(diffs, times, limit, level):
+    temp = 0
+    for n in range(len(diffs)):
+        if n == 0:
+            temp = temp + times[0]
+        else:
+            if diffs[n] <= level:
+                temp = temp + times[n]
+            else:
+                temp = temp + (diffs[n]-level)*times[n-1] +(diffs[n]-level)*times[n] + times[n]
+        if limit < temp:
+            return False
+    return True
+
+def solution(diffs, times, limit):
+    level = 0
+    
+    lv_min = 1
+    lv_max = max(diffs)
+    
+    answer = lv_max
+    
+    while lv_min <= lv_max:
+        mid = (lv_min + lv_max) // 2
+        level += 1
+        
+        if checkLimit(diffs, times, limit, mid):
+            answer = mid
+            lv_max = mid - 1
+        else:
+            lv_min = mid + 1
+            
+    return answer
