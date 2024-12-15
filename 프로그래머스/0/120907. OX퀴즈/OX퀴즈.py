4cd08dfe9f56b458117f1ff014ea728f86e41c6c
behindy <solme47@gmail.com>
2024-12-15 10:39:03 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,16 +1,2 @@
 def solution(quiz):
-    answer = []
-    for i in range(len(quiz)):
-        operator = quiz[i].split(' ')[1]
-        result = int(quiz[i].split(' ')[4])
-        if operator == '+':
-            if result == int(quiz[i].split(' ')[0]) + int(quiz[i].split(' ')[2]):
-                answer.append('O')
-            else:
-                answer.append('X')
-        else:
-            if result == int(quiz[i].split(' ')[0]) - int(quiz[i].split(' ')[2]):
-                answer.append('O')
-            else:
-                answer.append('X')
-    return answer
+    return ['O' if eval(eq) == int(result) else 'X' for eq, result in [q.split('=') for q in quiz] ]
