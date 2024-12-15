704c5fca291113e50dc8c5d95c476e5c1a4502cb
behindy <solme47@gmail.com>
2024-12-15 10:25:58 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,17 +1,15 @@
 def solution(quiz):
     answer = []
     for i in range(len(quiz)):
-        left = int(quiz[i].split(' ')[0])
         operator = quiz[i].split(' ')[1]
-        right = int(quiz[i].split(' ')[2])
         result = int(quiz[i].split(' ')[4])
         if operator == '+':
-            if result == left + right:
+            if result == int(quiz[i].split(' ')[0]) + int(quiz[i].split(' ')[2]):
                 answer.append('O')
             else:
                 answer.append('X')
         else:
-            if result == left - right:
+            if result == int(quiz[i].split(' ')[0]) - int(quiz[i].split(' ')[2]):
                 answer.append('O')
             else:
                 answer.append('X')
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 10.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 15일 10:23:23
+2024년 12월 15일 10:25:53
 
 ### 문제 설명
 
