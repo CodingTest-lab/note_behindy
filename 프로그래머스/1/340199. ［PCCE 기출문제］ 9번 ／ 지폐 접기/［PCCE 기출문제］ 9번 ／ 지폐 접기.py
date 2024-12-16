928de2b6f347c875e89dbe009caeb24ab2e828d2
behindy <solme47@gmail.com>
2024-12-16 16:06:59 +0900

    Update ［PCCE 기출문제］ 9번 ／ 지폐 접기.py

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -1,9 +1,8 @@
-def solution(wallet, bill):
-    answer = 0
+def solution(wallet, bill, count = 0):
     bill = sorted(bill)
     
     while min(wallet) < min(bill) or max(wallet) < max(bill):
         bill = [min(bill), int(max(bill)/2)]
-        answer += 1
+        count += 1
         
-    return answer
+    return count
