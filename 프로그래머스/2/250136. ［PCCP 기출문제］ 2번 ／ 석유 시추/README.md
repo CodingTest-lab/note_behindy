5b5cd567c8d5e289fd686971644235c12ea62eb5
behindy <solme47@gmail.com>
2024-12-17 09:35:50 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 518.19 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3) 
 
 ### 성능 요약
 
-메모리: 40.6 MB, 시간: 491.14 ms
+메모리: 46.3 MB, 시간: 518.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 17일 09:27:05
+2024년 12월 17일 09:35:46
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,40 +1,52 @@
-def find_cluster(land, row, col, visited):
-    if (row < 0 or row >= len(land) or 
-        col < 0 or col >= len(land[0]) or 
-        land[row][col] == 0 or 
-        (row, col) in visited):
-        return 0
+def find_cluster(land, start_row, start_col, visited):
+    # 이미 방문한 클러스터라면 None 반환
+    if (start_row, start_col) in visited:
+        return None
     
-    size = 1
-    visited.add((row, col))
-    affected_cols = {col}  # 영향을 받는 열들
-    stack = [(row, col)]
+    cluster = set()  # 현재 클러스터의 모든 좌표를 저장
+    stack = [(start_row, start_col)]
+    cluster.add((start_row, start_col))
     
     while stack:
-        curr_row, curr_col = stack.pop()
-        
-        # 4방향 탐색을 한번에 처리
-        for nr, nc in [(curr_row-1, curr_col), (curr_row+1, curr_col),
-                       (curr_row, curr_col-1), (curr_row, curr_col+1)]:
-            if (0 <= nr < len(land) and 0 <= nc < len(land[0]) and 
-                land[nr][nc] == 1 and (nr, nc) not in visited):
-                stack.append((nr, nc))
-                visited.add((nr, nc))
-                affected_cols.add(nc)
-                size += 1
+        row, col = stack.pop()
+        # 4방향 탐색
+        for next_row, next_col in [
+            (row-1, col), (row+1, col),
+            (row, col-1), (row, col+1)
+        ]:
+            # 유효한 범위이고, 석유가 있고, 아직 클러스터에 포함되지 않은 지점이면
+            if (0 <= next_row < len(land) and 
+                0 <= next_col < len(land[0]) and 
+                land[next_row][next_col] == 1 and 
+                (next_row, next_col) not in cluster):
+                stack.append((next_row, next_col))
+                cluster.add((next_row, next_col))
     
-    return size, affected_cols
+    # 클러스터의 좌표들과 크기를 반환
+    return cluster, len(cluster)
 
 def solution(land):
-    visited = set()
-    column_sizes = [0] * len(land[0])
+    n, m = len(land), len(land[0])
+    visited = set()  # 전체 방문 좌표
+    oil_by_column = [0] * m  # 각 열의 석유량
     
-    for col in range(len(land[0])):
-        for row in range(len(land)):
-            if land[row][col] == 1 and (row, col) not in visited:
-                size, affected_cols = find_cluster(land, row, col, visited)
-                # 클러스터가 영향을 미치는 모든 열에 크기를 더함
-                for ac in affected_cols:
-                    column_sizes[ac] += size
+    # 모든 지점을 순회
+    for row in range(n):
+        for col in range(m):
+            # 석유가 있는 지점을 발견하면
+            if land[row][col] == 1:
+                # 클러스터 탐색 시작
+                result = find_cluster(land, row, col, visited)
+                
+                # 이미 방문한 클러스터가 아니라면
+                if result is not None:
+                    cluster_coords, cluster_size = result
+                    # 방문 집합에 클러스터 좌표들 추가
+                    visited.update(cluster_coords)
+                    
+                    # 클러스터가 지나는 모든 열에 크기 기록
+                    affected_cols = set(coord[1] for coord in cluster_coords)
+                    for col_idx in affected_cols:
+                        oil_by_column[col_idx] += cluster_size
     
-    return max(column_sizes)
+    return max(oil_by_column)
