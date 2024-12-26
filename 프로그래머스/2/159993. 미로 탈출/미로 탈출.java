672b80fa6fe631fd534ea18837c3fee80c99cbb1
behindy <solme47@gmail.com>
2024-12-27 06:44:57 +0900

    Update 미로 탈출.java

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -36,30 +36,39 @@ class Solution {
         queue.offer(new int[]{start[0], start[1]});
         visited[start[0]][start[1]] = 1;
         
-        // 
+        // 탐색 시작
         while(!queue.isEmpty()) {
             int[] curr = queue.poll();
             
+            // 종료지점에 도달한 상태인지 확인
             if(curr[0] == end[0] && curr[1] == end[1]) {
                 return visited[curr[0]][curr[1]] - 1;
             }
             
+            // 4 방향으로 이동 시도
             for(int i = 0; i < 4; i++) {
                 int nx = curr[0] + dx[i];
                 int ny = curr[1] + dy[i];
                 
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m 
-                   && maps[nx].charAt(ny) != 'X' 
-                   && visited[nx][ny] == 0) {
+                // 이동을 시도하고 좌표의 타당함을 검사 
+                if(nx >= 0 && nx < n  // 새로운 수평좌표가 격자 안에 존재하는지
+                   && ny >= 0 && ny < m  // 새로운 수직좌표가 격자 안에 존재하는지
+                   && maps[nx].charAt(ny) != 'X' //새로운 좌표가 벽에 위치하는지
+                   && visited[nx][ny] == 0) // 이미 방문한곳인지
+                {
+                    // 검사를 통과한 경우에 방문했다고 표시, 경로에 추가.
                     visited[nx][ny] = visited[curr[0]][curr[1]] + 1;
                     queue.offer(new int[]{nx, ny});
                 }
             }
         }
+        
+        // while 문에서 함수가 반환되지 못한 경우 -> 목표로의 도달이 불가능한 상태
         return -1;
     }
     
     // target (S 시작, L 레버, E 도착)
+    // maps 에서 target의 좌표를 추출하는 함수
     private int[] findObject(String[] maps, char target){
         // 지도(maps) 순회를 통해 target을 탐색
         for(int i = 0; i<maps.length; i++){
